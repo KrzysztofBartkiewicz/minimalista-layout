2e57fe8c2c35aa1eaf7282820004d85b6ea5710a
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-06-11 10:33:29 +0200

    added nav animation

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,11 @@ import '../scss/main.scss';
 /* place your code below */
 
 const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
 
-hamburger.addEventListener('click', () => {
+const handleClick = () => {
    hamburger.classList.toggle('hamburger--active');
-});
+   nav.classList.toggle('visible');
+}
+
+hamburger.addEventListener('click', handleClick);
